ccdd85a45df5dbf25d1254ecbe4eb4b96834ab14
Brian Rowe <brian@Brians-iMac.local>
2015-07-27 20:20:44 -0400

    Initial commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to com.creeaaakk.blockhead
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/com/creeaaakk/blockhead_test.clj
@@ -0,0 +1,7 @@
+(ns com.creeaaakk.blockhead-test
+  (:require [clojure.test :refer :all]
+            [com.creeaaakk.blockhead :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
